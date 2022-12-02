8d207504f5f3f16a8ef1e3409dfa5b909bdb99ca
Alexandre <Alexandre@MacBook-Pro-Alex.local>
2022-12-02 11:08:14 +0100

    SRC CONTROLLER

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,24 +9,16 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/')]
 class HomeController extends AbstractController
 {
-<<<<<<< HEAD
     #[Route('', name: 'app_home')]
     public function index(): Response
     {
         $array = [
-          'hello' => 'world',
+            'hello' => 'world',
             'hello2' => 'world2',
         ];
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'array' => $array,
-=======
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
->>>>>>> alex
         ]);
     }
 }
