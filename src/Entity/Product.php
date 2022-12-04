be0548a7b83b98da45c0c086ad4580dda30a8b65
Youns29 <109031854+Youns29@users.noreply.github.com>
2022-12-04 17:45:05 +0100

    DEBUG PRODUCT :

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -108,4 +108,29 @@ class Product
 
         return $this;
     }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
 }
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,9 +4,9 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Product;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,16 +15,25 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('Description', TextType::class,
-                [ "label" => "Description du produit"])
-            ->add('Price')
-            ->add('quantity')
-            ->add('category', EntityType::class, [
-                "class" => Category::class,
-                "label" => "name"
+            ->add('title', TextType::class, [
+                'label' => "Titre"
             ])
-            ->add('seller')
+            ->add('description', TextType::class, [
+                'label' => "Description"
+            ])
+            ->add('price', TextType::class, [
+                'label' => "Prix"
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'label' => 'Catégorie',
+                'choice_label' => 'name',
+                'choice_value' => 'id'
+            ])
+ /*           ->add('image', TextType::class, [
+                'label' => "Image"
+            ])*/;
+
         ;
     }
 
